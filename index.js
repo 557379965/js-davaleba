0e1570eb0a3e97e730a2174f1ea2de801e2f6a22
gantsa <tskhadadze.gvanca@bk.ru>
2023-11-24 22:27:15 +0400

    feat: init project

---
+++ index.js
@@ -0,0 +1,35 @@
+let numbers = [15,16,17,18,19,20];
+
+for(let i = 0; i < numbers.length; i++){
+    if(i % 2 == 0)
+    console.log(numbers[i])
+}
+
+
+for(let i = 0; i < numbers.length; i++){
+  if(i % 2 !== 0)
+    console.log(numbers[i])
+}
+
+let kentiArray = [];
+let luwiArray = [];
+
+
+for( let i = 0; i < numbers.length; i++ ){
+ if(i % 2 == 0){
+     kentiArray.push(numbers[i])
+  }
+  else{luwiArray.push(numbers[i])}
+}
+console.log(kentiArray,luwiArray)
+
+
+
+
+
+let array = [15,16,17,18,19,20];
+let sum = 0;
+for (let i = 0; i < array.length; i++){
+  sum += array[i]
+  console.log(sum/array.length)
+}
